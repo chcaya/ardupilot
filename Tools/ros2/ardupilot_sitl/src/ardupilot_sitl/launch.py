923bf0aec6d1c5446e2a278f760b2f6cc8c77372
chcaya <cayc2401@usherbrooke.ca>
2025-01-20 15:15:44 -0500

    Deleted baudrate

--- Tools/ros2/ardupilot_sitl/src/ardupilot_sitl/launch.py
+++ Tools/ros2/ardupilot_sitl/src/ardupilot_sitl/launch.py
@@ -314,7 +314,6 @@ class MAVProxyLaunch:
             "127.0.0.1:14551 ",
             f"--master {master} ",
             f"--sitl {sitl} ",
-            f"--baudrate {baudrate}",
             "--non-interactive ",
         ]
 
